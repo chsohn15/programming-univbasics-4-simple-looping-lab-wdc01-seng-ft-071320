184608e0b75d3b8ac0a71224cbed4a334d4eae49
Christina Sohn <chsohn15@gmail.com>
2020-06-01 00:24:50 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,13 @@
 # Write your methods here
+
+def loop_message_five_times (string)
+  puts string
+  puts string
+  puts string
+  puts string
+  puts string
+end
+
+def loop_message_n_times(string, integer)
+  integer.times puts string 
+end
